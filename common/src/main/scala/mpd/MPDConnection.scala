d0c490f8a44c87de7543ea60491d1883188235dc
Andrée Ekroth <andree.ekroth@gmail.com>
2013-06-21 16:45:34 +0200

    Minor MPDC fixes

--- common/src/main/scala/mpd/MPDConnection.scala
+++ common/src/main/scala/mpd/MPDConnection.scala
@@ -9,13 +9,13 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.io.BufferedReader
 
-object MPDInternal extends MPDInternalTypes
+final object MPDInternal extends MPDInternalTypes
 trait MPDInternalTypes {
   val OK = "OK"
   val ACK = "ACK"
   val BuffSize = 1024
   val OKConnect = """OK MPD (.*)""".r
-  val ACKConnect = """ACK \[(.*)@(.*)] {(.*)}(.*)""".r
+  val ACKConnect = """ACK [(.*)@(.*)] \{(.*)\}(.*)""".r
 
   sealed trait Fail
   case class Disconnected() extends Fail
@@ -26,24 +26,21 @@ trait MPDInternalTypes {
 import Scalaz._
 import MPDInternal._
 
-object MPDConnection {
+final object MPDConnection {
   def connect(addr: String, port: Int): Fail \/ MPDConnection = {
     try {
       val socket = new JSocket(addr, port)
       val in = new BufferedReader(new InputStreamReader(socket.getInputStream), BuffSize)
       val line = in.readLine
-      in.close
-      OKConnect.findFirstIn(line) match {
-	case OKConnect(ver) => {
-	  val out = new OutputStreamWriter(socket.getOutputStream, "UTF-8")
-	  val in = new InputStreamReader(socket.getInputStream, "UTF-8")
+      line match {
+        case OKConnect(ver) => {
+          val out = new OutputStreamWriter(socket.getOutputStream, "UTF-8")
+          val in = new InputStreamReader(socket.getInputStream, "UTF-8")
 
-	  MPDConnection(socket, in, out).right
-	}
-	case _ => ACKConnect.findFirstIn(line) match {
-	  case x@ACKConnect(err, cmdNum, cmd, msg) => Unknown(x.toString).left
-	  case _ => Unknown("unknown").left
-	}
+          MPDConnection(socket, in, out).right
+        }
+        case x @ ACKConnect(err, cmdNum, cmd, msg) => Unknown(x.toString).left
+        case x => Unknown(s"msg: $x").left
       }
     } catch {
       case e: Throwable => Unknown(e.toString).left
@@ -51,49 +48,50 @@ object MPDConnection {
   }
 }
 
-case class MPDConnection(socket: JSocket, input: InputStreamReader, output: OutputStreamWriter) {
+final case class MPDConnection(socket: JSocket, input: InputStreamReader, output: OutputStreamWriter) {
+  val CmdBegin = "command_list_begin"
+  val CmdEnd = "command_list_end"
+
   def connected: Fail \/ Unit =
     if (socket.isConnected && !socket.isClosed()) ().right
     else Disconnected().left
 
-  def write(cmd: String): Fail \/ Unit = {
-    def write = try {
-      output.write(cmd)
-      output.flush.right
-    } catch {
-      case e: Throwable => Unknown(e.toString).left
-    }
+  def batch[T](f: => T): Fail \/ T = {
 
-    for (
-      _ <- connected;
-      o <- write
-    ) yield o
+    for {
+      _ <- write(CmdBegin)
+      v = f
+      _ <- write(CmdEnd)
+      _ = output.flush
+    } yield v
   }
 
+  def write(cmd: String): Fail \/ Unit = try {
+    output.write(s"""$cmd\n""").right
+  } catch {
+    case e: Throwable => Unknown(e.toString).left
+  }
+
+  def flush = output.flush
+
   def read(): Fail \/ Vector[String] = {
-    @tailrec def readEnd(br: BufferedReader, out: Vector[String]): Fail \/ Vector[String] = {
-      val line = br.readLine
-
-      line match {
-	case null => Unknown("empty read").left
-	case x if (x contains "ACK") => ACKd().left
-	case x if (x contains "OK") => out.right
-	case _ => readEnd(br, out :+ line)
+    @tailrec def readEnd(br: BufferedReader, out: Vector[String]): Fail \/ Vector[String] =
+      br.readLine match {
+        case null => Unknown("empty read").left
+        case x if (x contains "ACK") => ACKd().left
+        case x if (x contains "OK") => out.right
+        case x => readEnd(br, out :+ x)
       }
-    }
 
-    for (
-      _ <- connected;
-      o <- readEnd(new BufferedReader(input, BuffSize), Vector.empty)
-    ) yield o
+    readEnd(new BufferedReader(input, BuffSize), Vector.empty)
   }
 
   def disconnect(): Fail \/ Unit = {
     if (connected.isRight) {
       try {
-	socket.close.right
+        socket.close.right
       } catch {
-	case e: Throwable => Unknown(e.toString).left
+        case e: Throwable => Unknown(e.toString).left
       }
     } else ().right
   }
