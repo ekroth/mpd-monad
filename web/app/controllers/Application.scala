3193886d62613cb5ba5a8181716e8e8e3b390d6e
meris <meris.bahti@gmail.com>
2013-06-22 21:16:10 +0200

    add support for new mpd-protocol wrapper to webGUI

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -2,13 +2,46 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import mpd._
+import mpd.std._
+import mpd.messages._
 
 object Application extends Controller {
 
+  import scalaz._
+  import Scalaz._
 
-  def index = Action {
-    Ok(views.html.index("heyho"))
-  }
+  val srv = new ServerMessagesStd with MpdComponentSync with ServerMessagesDebug with PlaybackMessagesStd
   
+  val connection = srv.mpd.connect("192.168.1.2",6600)
+
+  def index(cmd: String = "index") = Action {
+    Ok(views.html.index(cmd))
+  }
+ 
+  def issueCmd[T](cmd: => T) = { 
+    cmd
+    Redirect("/")
+  } 
+
+  def pley = Action {
+    issueCmd(srv.play())
+  }
+
+  def next = Action {
+    issueCmd(srv.next)
+  }
+
+  def prev = Action {
+    issueCmd(srv.previous)
+  }
+
+  def stop = Action {
+    issueCmd(srv.stop)
+  }
+
+  def pause = Action {
+    issueCmd(srv.pause())
+  }
 
 }
