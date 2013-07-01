39f5f6a155bebea373111e540d93557da8e09ce9
Andrée Ekroth <andree.ekroth@gmail.com>
2013-07-01 23:47:21 +0200

    ServerMsg raw overloads

--- common/src/main/scala/mpd/messages/Server.scala
+++ common/src/main/scala/mpd/messages/Server.scala
@@ -5,9 +5,20 @@ import scala.concurrent.Future
 
 trait ServerMsg {
   this: ExecutorComponent =>
+
   def raw(s: String): Future[Unit]
+
+  final def raw(s: String, a1: String): Future[Unit] = raw(s"""$s "$a1"""")
+
+  final def raw(s: String, a1: String, as: String*): Future[Unit] = {
+    val args = (a1 +: as) map { x => s""""$x"""" } mkString(" ")
+    raw(s"""$s $args""")
+  }
+
   def read(): Future[Vector[String]]
+
   def clear(): Future[Unit]
+
   def wread(s: String) = for {
     _ <- clear
     _ <- raw(s)
