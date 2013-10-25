b191402593f0a4f4510c7f1d6354b38fd4df8693
Andrée Ekroth <andree.ekroth@gmail.com>
2013-10-25 19:54:19 +0200

    Playlist support

---
+++ common/src/main/scala/mpd/Playlist.scala
@@ -0,0 +1,30 @@
+package mpd
+
+import scalaz._
+
+trait Playlist {
+   def playlistinfo()(implicit b: Base) = {
+    @tailrec def grouper[T <: Tuple2[_, _]](xs: Seq[T],
+					    delim: Option[T],
+					    ys: Seq[Traversable[T]] = Seq.empty): Seq[Traversable[T]] = {
+      if (xs.isEmpty || delim.isEmpty) ys
+      else {
+        val take = (xs.tail takeWhile { _._1 != delim.get._1 }) :+ xs.head
+        grouper(xs.drop(take.length), delim, ys :+ take)
+      }
+    }
+
+    b.wread("playlistinfo") map { s =>
+      val vpairs = MpdParse.valuePairs(s)
+      val pairs = grouper(vpairs, vpairs.headOption)
+      for (p <- pairs) yield MpdParse.parseSong(p.toMap)
+    }
+  }
+
+}
+
+trait PlaylistInstances {
+  implicit val playlistImplicit = new Playlist { }
+}
+
+final object PlaylistInstances extends PlaylistInstances
