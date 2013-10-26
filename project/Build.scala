8eb01cdd463732705d99567bc948253c4fdea922
Andrée Ekroth <andree.ekroth@gmail.com>
2013-10-26 18:32:50 +0200

    add android to build

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,6 @@
 import sbt._
 import Keys._
+import AndroidKeys._
 
 object build extends Build {
   val projName = "mpd"
@@ -7,7 +8,7 @@ object build extends Build {
     scalaVersion := "2.10.1",
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"))
 
-  val common = {
+  lazy val common = {
     val name = "common"
 
     Project(
@@ -16,10 +17,10 @@ object build extends Build {
       settings = Defaults.defaultSettings ++ sharedSettings ++ Seq(
 	resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/snapshots/",
 	libraryDependencies ++= Seq("org.scalaz" %% "scalaz-core" % "7.0.0",
-				    "com.typesafe.akka" %% "akka-actor" % "2.1.0",
-				    "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"),
-	initialCommands += 
-	  """import mpd._ 
+	  "com.typesafe.akka" %% "akka-actor" % "2.1.0",
+	  "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"),
+	initialCommands +=
+	  """import mpd._
 	  object AllInstancesDebug extends AllInstances with DebugInstances
 	  import AllInstancesDebug._
 	
@@ -27,11 +28,49 @@ object build extends Build {
 
 	  val con = (implicitly[Base]).Connect("127.0.0.1", 6600)
 	  def useCon[T](f: mpd.MPDS => T) = for { m <- con } yield f(m)"""
-	)
       )
+    )
   }
 
-  val web = {
+  final object General {    
+    val settings = Defaults.defaultSettings ++ Seq (
+      name := "mpd-monad",
+      version := "0.1",
+      versionCode := 0,
+      scalaVersion := "2.10.1",
+      platformName in Android := "android-17",
+      javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6"),
+      scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
+      libraryDependencies ++= Seq(
+        "org.scaloid" %% "scaloid" % "2.3-8")
+    )
+    val proguardSettings = Seq (
+      useProguard in Android := true,
+      proguardOptimizations in Android := Seq(
+        "-dontoptimize")
+    )
+
+    val fullAndroidSettings =
+      General.settings ++
+    AndroidProject.androidSettings ++
+    TypedResources.settings ++
+    AndroidManifestGenerator.settings
+  }
+
+  lazy val mpdDroid = Project (
+    "mpd-droid",
+    file("android"),
+    settings = General.fullAndroidSettings ++ General.proguardSettings ++ AndroidNdk.settings
+  ).dependsOn(common)
+
+  lazy val mpdDroidFast = Project (
+    "mpd-droid-fast",
+    file("android"),
+    settings = General.fullAndroidSettings ++ AndroidNdk.settings ++ Seq(useProguard in Android := false)
+  ).dependsOn(common)
+
+  
+  lazy val web = {
     import play.Project._
     val name = "web"
     val appVersion = "1.0-SNAPSHOT"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,3 +6,6 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "2.1.1")
+
+// Android
+addSbtPlugin("org.scala-sbt" % "sbt-android-plugin" % "0.6.2")
